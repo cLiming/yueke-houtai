7e56b061c85eab45643a63f7122f4a4d4297c4da
corazon <corazon>
2020-11-10 17:53:44 +0800

    shiro5

---
+++ src/main/java/com/yuekehoutai/StartApplication.java
@@ -0,0 +1,13 @@
+package com.yuekehoutai;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.yuekehoutai.mapper")
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
