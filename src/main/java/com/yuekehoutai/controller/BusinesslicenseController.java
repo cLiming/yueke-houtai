a1947e79502a9b2baa60af0278060809f4c34142
qts123 <614023661@qq.com>
2020-11-26 20:20:41 +0800

    busicense1

--- src/main/java/com/yuekehoutai/controller/BusinesslicenseController.java
+++ src/main/java/com/yuekehoutai/controller/BusinesslicenseController.java
@@ -1,9 +1,15 @@
 package com.yuekehoutai.controller;
 
 
+import com.yuekehoutai.service.BusinesslicenseService;
+import com.yuekehoutai.util.JsonResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -13,9 +19,15 @@ import org.springframework.stereotype.Controller;
  * @author corazon
  * @since 2020-11-26
  */
-@Controller
+@RestController
 @RequestMapping("/businesslicense")
 public class BusinesslicenseController {
-
+    @Resource
+    private BusinesslicenseService businesslicenseService;
+    @PostMapping("/updateStatus")
+    public JsonResult updateStatus(Integer id){
+        businesslicenseService.updateStatus(id);
+        return  new JsonResult(200,"success",null,null);
+    }
 }
 
--- src/main/java/com/yuekehoutai/service/Impl/BusinesslicenseServiceImpl.java
+++ src/main/java/com/yuekehoutai/service/Impl/BusinesslicenseServiceImpl.java
@@ -1,10 +1,13 @@
 package com.yuekehoutai.service.Impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.yuekehoutai.domain.Businesslicense;
 import com.yuekehoutai.mapper.BusinesslicenseMapper;
 import com.yuekehoutai.service.BusinesslicenseService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +19,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BusinesslicenseServiceImpl extends ServiceImpl<BusinesslicenseMapper, Businesslicense> implements BusinesslicenseService {
-
+    @Resource
+    private BusinesslicenseMapper businesslicenseMapper;
+    @Override
+    public void updateStatus(Integer id) {
+        businesslicenseMapper.update(new Businesslicense(),new UpdateWrapper<Businesslicense>().set("status",1).eq("b_id",id));
+    }
 }
