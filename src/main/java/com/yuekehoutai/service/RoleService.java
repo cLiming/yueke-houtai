235a8b524acd5a9a61aa104b33789d0e46b0115e
qts123 <614023661@qq.com>
2020-11-26 14:12:10 +0800

    jiaose1

--- src/main/java/com/yuekehoutai/service/RoleService.java
+++ src/main/java/com/yuekehoutai/service/RoleService.java
@@ -3,6 +3,8 @@ package com.yuekehoutai.service;
 import com.yuekehoutai.domain.Role;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-11-10
  */
 public interface RoleService extends IService<Role> {
-
+    public List<Role> selectRole();
 }
