e7a70ec3a4e52164cae4a3bb1badf4687796b9f1
qts123 <614023661@qq.com>
2020-11-27 14:27:00 +0800

    busicense3

--- src/main/java/com/yuekehoutai/service/BusinesslicenseService.java
+++ src/main/java/com/yuekehoutai/service/BusinesslicenseService.java
@@ -3,6 +3,8 @@ package com.yuekehoutai.service;
 import com.yuekehoutai.domain.Businesslicense;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -13,4 +15,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface BusinesslicenseService extends IService<Businesslicense> {
     void updateStatus(Integer id);
+    List<Businesslicense> selectAll();
+    void deleteById(Integer id);
 }
